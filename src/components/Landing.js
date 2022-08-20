aa8e84ed60880ae3af5a46d2e70fc0bb4a57e67e
Sahil <aggarwalsahil2004@gmail.com>
2022-08-20 18:20:41 +0530

    image optimisation

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,24 +1,23 @@
 import React from 'react'
-import poster from "../images/Landing.png";
+import { Link } from 'react-router-dom'
+import poster from "../images/landing.webp";
 
 const Landing = () => {
     return (
-        <>
-            <div className='bg-[url("images/background.jpg")] bg-no-repeat bg-center bg-cover fixed -z-10 w-full h-screen' >
-                <div className=' flex my-32 mx-44'>
-                    <div className='m-10'>
-                        <h1 className='text-4xl font-bold'>Donate!!</h1>
-                        <p className='text-xl my-4'>Be part of a good cause. Show some love and donate to the society.</p>
+        <div className='bg-[url("images/background.webp")] bg-no-repeat bg-center bg-cover fixed -z-10 w-full h-screen' >
+            <div className='flex my-32 mx-44'>
+                <div className='m-10'>
+                    <h1 className='text-4xl font-bold'>Donate!</h1>
+                    <p className='text-xl my-4'>Be part of a good cause. Show some love and donate to the society.</p>
 
-                        <button className="font-semibold btn bg-gradient-to-r from-gradient1a to-gradient1b border rounded-lg px-4 py-2 m-2">Register as Donor</button>
-                        <button className="font-semibold btn bg-gradient-to-r from-gradient1a to-gradient1b border rounded-lg px-4 py-2 m-2">Register as NGO</button>
-                    </div>
-                    <div className='w-1/2'>
-                        <img className=' duration-300 hover:translate-y-1 outline-offset-1' src={poster} alt="" />
-                    </div>
+                    <Link to='/user/signup' className="font-semibold btn bg-gradient-to-r from-gradient1a to-gradient1b border rounded-lg px-4 py-2 m-2">Register as Donor</Link>
+                    <Link to='/ngo/signup' className="font-semibold btn bg-gradient-to-r from-gradient1a to-gradient1b border rounded-lg px-4 py-2 m-2">Register as NGO</Link>
+                </div>
+                <div className='w-1/2'>
+                    <img className='duration-300 hover:translate-y-1 outline-offset-1' src={poster} alt="" />
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
