3eb618995b7d70e6b1f5a139b57475aeb7ffae69
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-08-20 21:09:59 +0530

    SA4

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,10 +9,11 @@ const Landing = () => {
         <>
             <div>
                 <div className="flex border-b-4 drop-shadow-lg justify-between">
-                    <div className='flex m-4 '>
-                        <Logo dimension={2} />
-                        <p className='flex items-center font-bold text-xl justify-center mx-3'>DoNation</p>
-                    </div>
+                    <Link to="/">
+                        <div className='flex m-4'>
+                            <Logo dimension={2} />
+                            <p className='flex items-center font-bold text-xl justify-center mx-3'>DoNation</p></div></Link>
+
                     <div className='flex justify-center items-center'>
                         <ul className='flex'>
                             <li className='mx-4 -my-2 font-semibold hover:underline cursor-pointer'>Home</li>
--- src/components/User/Navbar.js
+++ src/components/User/Navbar.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import Logo from "../Logo";
 import User from "../../images/user-interface.png";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div>
-        <div className="flex border-b-4 drop-shadow-lg justify-between">
-          <div className='flex m-4 '>
-            <Logo dimension={2}/>
-            <p className='flex items-center font-bold text-xl justify-center mx-3'>DoNation</p>
-            </div>
-            <div className='flex justify-center items-center'>
-              <ul className='flex'>
-                <li className='mx-4 -my-2 font-semibold hover:underline cursor-pointer'>Home</li>
-                <li className='mx-4 -my-2 font-semibold hover:underline cursor-pointer'>Dashboard</li>
-                <li className='mx-4 -my-2 font-semibold hover:underline cursor-pointer'><img src={User} className="w-6" alt="" /></li>
-              </ul>
-            </div>
+                <div className="flex border-b-4 drop-shadow-lg justify-between">
+                    <Link to="/">
+                        <div className='flex m-4'>
+                            <Logo dimension={2} />
+                            <p className='flex items-center font-bold text-xl justify-center mx-3'>DoNation</p></div></Link>
+          <div className='flex justify-center items-center'>
+            <ul className='flex'>
+              <li className='mx-4 -my-2 font-semibold hover:underline cursor-pointer'>Home</li>
+              <li className='mx-4 -my-2 font-semibold hover:underline cursor-pointer'>Dashboard</li>
+              <li className='mx-4 -my-2 font-semibold hover:underline cursor-pointer'><img src={User} className="w-6" alt="" /></li>
+            </ul>
+          </div>
         </div>
     </div>
   )
